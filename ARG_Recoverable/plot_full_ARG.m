628b32f448de582392909fcc5e1ba1da3e224241
Elizabeth-Hayman <53257901+Elizabeth-Hayman@users.noreply.github.com>
2022-01-07 16:11:48 +0000

    Update plot_full_ARG.m

--- ARG_Recoverable/plot_full_ARG.m
+++ ARG_Recoverable/plot_full_ARG.m
@@ -1,31 +1,33 @@
+%Current configuration will plot the probability of full ARG detection conditional on 2 recombinations for a variety of theta.
+%Can be configured 
 clf
 tic
-R=2;
+
+R=2; %configurable params
 N0=0;
 n=50;
 rho=0.1;
-THETA=[10,100,1000,10^10];
+THETA=[10,100,1000,10^10]; %selection of theta. 10^10 ~ infinity
 z=.5;
 
 for theta= THETA
     P = solve_full_topology(R,n,N0,rho,theta,z);
-    [~, q] = solve_number_recombs(n, R, rho);
-    for n0 = 0:N0
-        probs = zeros(1, n);
-        
-        for nl=1:n
-            if q(nl,1) ==0
-                probs(nl) = 1;
-            else
-                probs(nl) = P(n0+1, nl, nl+1, 1, 1, 1, 1)/q(nl,1);
-            end
+    [~, q] = solve_number_recombs(n, R, rho); %probability a sample has R recombination, used for conditioning
+
+    probs = zeros(1, n); %initialise an empty probabilty vector each for loop
+    for nl=1:n
+        if q(nl,1) ==0 
+            probs(nl) = 0; %if loop prevent NaN entries: if p and q are zero, want the ratio to also be 0
+        else
+            probs(nl) = P(n0+1, nl, nl+1, 1, 1, 1, 1)/q(nl,1); %calculate the conditional probability for non-zero entries
         end
-        plot(probs, 'LineWidth',3);
-        hold on
-        xlim([0,n])
-        ylim([0,.2]);
     end
+    plot(probs, 'LineWidth', 3);
+    hold on
 end
+%Configure the graph
 legend('\theta=10', '\theta=100', '\theta=1000', '\theta=\infty')
-    %legend('n_0=0', 'n_0=1', 'n_0=2', 'n_0=3')
-toc
+xlim([1,n])
+ylim([0,.2]);
+
+toc %outputs run time
